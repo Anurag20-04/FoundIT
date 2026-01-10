15d1bd8764a3e84d8d36f433abccbca062e8c821
Anurag <gautamaman596@gmail.com>
2026-01-10 17:38:58 +0530

    fix missing path/fs imports

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
+const fs = require("fs");
 
 const connectDB = require("./config/dbconn.js");
 
