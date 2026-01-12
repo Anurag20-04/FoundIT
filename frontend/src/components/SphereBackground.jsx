f0e98dc7fbbdc5c0412a1a48eab0b4f65798673f
Anurag <gautamaman596@gmail.com>
2026-01-12 15:30:21 +0530

    browse button

--- frontend/src/components/SphereBackground.jsx
+++ frontend/src/components/SphereBackground.jsx
@@ -17,7 +17,7 @@ export default function SphereBackground() {
     <Particles
       id="tsparticles"
        style={{
-      pointerEvents: "none",   // 🔑 CRITICAL FIX
+      pointerEvents: "auto",   // 🔑 CRITICAL FIX
       position: "absolute",
       inset: 0,
       zIndex: 0,
