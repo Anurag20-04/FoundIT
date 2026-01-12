ba7ae39958ae9c22ebbcff67c7639989f005aa19
Anurag <gautamaman596@gmail.com>
2026-01-12 19:05:17 +0530

    mailer test

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -3,15 +3,13 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 module.exports = async function sendMail({ to, subject, html }) {
-  try {
-    return await resend.emails.send({
-      from: process.env.EMAIL_FROM, // "FoundIT <onboarding@resend.dev>"
-      to,
-      subject,
-      html,
-    });
-  } catch (err) {
-    console.error("❌ Mail server error:", err);
-    throw err;
-  }
+  console.log("RESEND KEY PRESENT:", !!process.env.RESEND_API_KEY);
+  console.log("EMAIL_FROM:", process.env.EMAIL_FROM);
+
+  return await resend.emails.send({
+    from: "FoundIT <onboarding@resend.dev>",
+    to: [to],
+    subject,
+    html,
+  });
 };
